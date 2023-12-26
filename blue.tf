4cc3d19b523e9beed0be07919ce821bc3599b881
Leonardwkwong <weikit.wong.2019@scis.smu.edu.sg>
2023-12-26 20:36:58 +0800

    Include aws load balancer target group and group attachment

--- blue.tf
+++ blue.tf
@@ -13,3 +13,24 @@ resource "aws_instance" "blue" {
     Name = "blue-${count.index}"
   }
 }
+
+resource "aws_lb_target_group" "blue" {
+  name     = "blue-tg-${random_pet.app.id}-lb"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    port     = 80
+    protocol = "HTTP"
+    timeout  = 5
+    interval = 10
+  }
+}
+
+resource "aws_lb_target_group_attachment" "blue" {
+  count            = length(aws_instance.blue)
+  target_group_arn = aws_lb_target_group.blue.arn
+  target_id        = aws_instance.blue[count.index].id
+  port             = 80
+}
