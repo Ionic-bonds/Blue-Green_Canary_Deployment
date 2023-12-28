dde9ef1e642c105979854cbc883cfe5c27a6dd76
Leonardwkwong <weikit.wong.2019@scis.smu.edu.sg>
2023-12-28 14:28:10 +0800

    Change subnets to subnet

--- main.tf
+++ main.tf
@@ -14,15 +14,15 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.19.0"
 
-  main = "main-vpc"
+  name = "main-vpc"
   cidr = var.vpc_cidr_block
 
   azs             = data.aws_availability_zones.available.names
-  private_subnets = slice(var.private_subnets_cidr_blocks, 0, var.private_subnet_count)
+  private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
+  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
 
   enable_nat_gateway = true
   enable_vpn_gateway = var.enable_vpn_gateway
-
 }
 
 module "app_security_group" {
@@ -63,11 +63,11 @@ resource "random_pet" "app" {
 }
 
 resource "aws_lb" "app" {
-  name               = "main-app-lb"
+  name               = "main-app-${random_pet.app.id}-lb"
   internal           = false
   load_balancer_type = "application"
   subnets            = module.vpc.public_subnets
-  security_groups    = [module.lb_security_group.this_security_group_id]
+  security_groups    = [module.lb_security_group.security_group_id]
 }
 
 resource "aws_lb_listener" "app" {
