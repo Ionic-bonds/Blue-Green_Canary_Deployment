46237698e0a51d1c708a4dcc9ed583a02757b4c2
Leonardwkwong <weikit.wong.2019@scis.smu.edu.sg>
2023-12-28 14:13:48 +0800

    formmatted variables.tf

--- variables.tf
+++ variables.tf
@@ -1,47 +1,47 @@
 variable "region" {
-    description = "The region Terraform deploys your instances"
-    type = string 
-    default = "us-west-2"
-  
+  description = "The region Terraform deploys your instances"
+  type        = string
+  default     = "us-west-2"
+
 }
 
 variable "vpc_cidr_block" {
-    description = "CIDR block for VPC"
-    type = string
-    default = "10.0.0.0/16"  
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "enable_vpn_gateway" {
-    description = "Enable a VPN gateway in your VPC."
-    type = bool 
-    default = false
+  description = "Enable a VPN gateway in your VPC."
+  type        = bool
+  default     = false
 }
 
 variable "public_subnet_count" {
-    description = "Number of public subnets."
-    type = number
-    default = 2
+  description = "Number of public subnets."
+  type        = number
+  default     = 2
 }
 
 variable "private_subnet_count" {
-    description = "Number of private subnets."
-    type = number
-    default = 2
+  description = "Number of private subnets."
+  type        = number
+  default     = 2
 }
 
 variable "private_subnet_cidr_blocks" {
-    description = "Available cidr blocks for public subnets"
-    type = list(string)
-    default = [ 
-        "10.0.1.0/24",
-        "10.0.2.0/24",
-        "10.0.3.0/24",
-        "10.0.4.0/24",
-        "10.0.5.0/24",
-        "10.0.6.0/24",
-        "10.0.7.0/24",
-        "10.0.8.0/24",        
-    ]
+  description = "Available cidr blocks for public subnets"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24",
+    "10.0.7.0/24",
+    "10.0.8.0/24",
+  ]
 }
 
 variable "private_subnet_cidr_blocks" {
