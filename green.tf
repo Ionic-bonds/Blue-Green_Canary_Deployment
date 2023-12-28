656f68d24f4f567e82f7d204a16346d07031ae7e
Leonardwkwong <weikit.wong.2019@scis.smu.edu.sg>
2023-12-28 14:29:18 +0800

    Debug and included vpc in vpc_id under lb target group

--- green.tf
+++ green.tf
@@ -18,7 +18,7 @@ resource "aws_lb_target_group" "green" {
   name     = "green-tg-${random_pet.app.id}-lb"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = module.vpc_id
+  vpc_id   = module.vpc.vpc_id
 
   health_check {
     port     = 80
